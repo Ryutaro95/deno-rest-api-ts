f6dfafc61993f1af49e35c5c37090d6e18f9acaa
Ryutaro95 <ryutaro9595@gmail.com>
2021-07-09 23:04:33 +0900

    create rest-api-deno

---
+++ deps.ts
@@ -0,0 +1,18 @@
+export {
+    Application,
+    Router,
+    Status,
+    Context,
+    isHttpError,
+} from "https://deno.land/x/oak@v7.7.0/mod.ts";
+
+export type { RouterContext } from "https://deno.land/x/oak@v7.7.0/mod.ts";
+
+export {
+    bold,
+    yellow,
+    green,
+    cyan,
+} from "https://deno.land/std@0.100.0/fmt/colors.ts";
+
+export { v4 as uuid } from "https://deno.land/std@0.100.0/uuid/mod.ts";
